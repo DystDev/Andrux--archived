be3b7e5a1a8677750b8e06da7c46b95c5d1d31b8
Rowan <rowan.h.marshall@gmail.com>
2020-07-30 18:50:46 +0100

    enemy Attak td

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,41 @@
+﻿using RPG.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+        Fighter fighter;
+        GameObject player;
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            player = GameObject.FindWithTag("Player");
+        }
+        private void Update()
+        {
+
+            if (InAttackRangeOfPlayer(player) && fighter.CanAttack(player))
+            {
+                fighter.Attack(player);
+                
+            }
+            else
+            {
+                fighter.Cancel();
+            }
+
+        }
+
+        private bool InAttackRangeOfPlayer(GameObject player)
+        {
+
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            return distanceToPlayer < chaseDistance;
+        }
+    }
+}
